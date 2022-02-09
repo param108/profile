23e3e2cbe2ba3c796d98e8c7235fba5d386e4b46
param108 <param.ponnaiyan@gmail.com>
2022-02-10 05:03:52 +0530

    Adding schema.sql file

---
+++ api/db/schema.sql
@@ -0,0 +1,110 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 12.9 (Ubuntu 12.9-0ubuntu0.20.04.1)
+-- Dumped by pg_dump version 12.9 (Ubuntu 12.9-0ubuntu0.20.04.1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: uuid-ossp; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
+
+
+--
+-- Name: EXTENSION "uuid-ossp"; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION "uuid-ossp" IS 'generate universally unique identifiers (UUIDs)';
+
+
+--
+-- Name: user_role; Type: TYPE; Schema: public; Owner: profile
+--
+
+CREATE TYPE public.user_role AS ENUM (
+    'user',
+    'admin'
+);
+
+
+ALTER TYPE public.user_role OWNER TO profile;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: schema_migrations; Type: TABLE; Schema: public; Owner: profile
+--
+
+CREATE TABLE public.schema_migrations (
+    version bigint NOT NULL,
+    dirty boolean NOT NULL
+);
+
+
+ALTER TABLE public.schema_migrations OWNER TO profile;
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: profile
+--
+
+CREATE TABLE public.users (
+    id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
+    handle text NOT NULL,
+    profile text,
+    role public.user_role,
+    owner uuid NOT NULL
+);
+
+
+ALTER TABLE public.users OWNER TO profile;
+
+--
+-- Name: schema_migrations schema_migrations_pkey; Type: CONSTRAINT; Schema: public; Owner: profile
+--
+
+ALTER TABLE ONLY public.schema_migrations
+    ADD CONSTRAINT schema_migrations_pkey PRIMARY KEY (version);
+
+
+--
+-- Name: users users_handle_key; Type: CONSTRAINT; Schema: public; Owner: profile
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_handle_key UNIQUE (handle);
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: profile
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: users_on_handle; Type: INDEX; Schema: public; Owner: profile
+--
+
+CREATE INDEX users_on_handle ON public.users USING btree (handle);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
