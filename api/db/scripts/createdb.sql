df070b6ca008aa54e207f8ff9b7c45afac864799
param108 <param.ponnaiyan@gmail.com>
2023-05-11 21:55:48 +0530

    Need to make db user the owner as well
    needed for migrate to work

--- api/db/scripts/createdb.sql
+++ api/db/scripts/createdb.sql
@@ -2,5 +2,6 @@ CREATE USER profile WITH password 'profile';
 ALTER USER profile WITH login;
 CREATE DATABASE profile;
 GRANT ALL ON DATABASE profile to profile;
+ALTER DATABASE profile owner to profile;
 \c profile
 CREATE EXTENSION "uuid-ossp";
