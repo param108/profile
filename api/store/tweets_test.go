a452d332d6d0830ff2f6128a3f8ab8d09b0e6448
param108 <param.ponnaiyan@gmail.com>
2023-05-17 16:37:44 +0530

    Update tweet working

--- api/store/tweets_test.go
+++ api/store/tweets_test.go
@@ -97,4 +97,105 @@ The first #tweet is a short
 			foundTags, "invalid tweet found")
 
 	})
+
+	t.Run("Update tweet with more tags", func(t *testing.T) {
+		tw, tags, err := testDB.(*StoreImpl).UpdateTweet(userID,
+			oldTweetID,
+			`#display
+The first #tweet is a short
+#Hello #World #Tree.`, "", tweetWriter)
+		assert.Nil(t, err, "failed to insert tweets and tags")
+		assert.NotNil(t, tw.CreatedAt, "empty created_at")
+		assert.NotEmpty(t, tw.ID, "ID is empty")
+
+		// A new tweet should have been inserted with a new ID
+		assert.Equal(t, oldTweetID, tw.ID, "same id returned for tweet")
+		assert.Equal(t, 4, len(tags), "incorrect number of tags")
+		for _, tag := range tags {
+			assert.NotEmpty(t, tag.ID, "empty ID")
+			assert.NotNil(t, tag.CreatedAt, "empty created at")
+		}
+	})
+
+	t.Run("Get all the Tags check there are only 4", func(t *testing.T) {
+		tags, err := testDB.GetTags(userID, tweetWriter)
+		assert.Nil(t, err, "failed to get tags")
+
+		assert.Equal(t, 4, len(tags), "incorrect number of tags")
+
+		foundTags := []string{}
+		for _, tag := range tags {
+			foundTags = append(foundTags, tag.Tag)
+		}
+
+		assert.Equal(t, []string{"hello", "tree", "tweet", "world"},
+			foundTags, "invalid tweet found")
+	})
+
+	t.Run("Check number of tweetTags", func(t *testing.T) {
+		tags, err := testDB.GetTweetTags(userID, oldTweetID, tweetWriter)
+		assert.Nil(t, err, "failed to get tags")
+
+		assert.Equal(t, 4, len(tags), "incorrect number of tags")
+
+		foundTags := []string{}
+		for _, tag := range tags {
+			foundTags = append(foundTags, tag.Tag)
+		}
+
+		assert.Equal(t, []string{"hello", "tree", "tweet", "world"},
+			foundTags, "invalid tweet found")
+
+	})
+
+	t.Run("Update tweet with less tags", func(t *testing.T) {
+		tw, tags, err := testDB.(*StoreImpl).UpdateTweet(userID,
+			oldTweetID,
+			`#display
+The first is a short
+#Hello #World #Tree.`, "", tweetWriter)
+		assert.Nil(t, err, "failed to insert tweets and tags")
+		assert.NotNil(t, tw.CreatedAt, "empty created_at")
+		assert.NotEmpty(t, tw.ID, "ID is empty")
+
+		// A new tweet should have been inserted with a new ID
+		assert.Equal(t, oldTweetID, tw.ID, "same id returned for tweet")
+		assert.Equal(t, 3, len(tags), "incorrect number of tags")
+		for _, tag := range tags {
+			assert.NotEmpty(t, tag.ID, "empty ID")
+			assert.NotNil(t, tag.CreatedAt, "empty created at")
+		}
+	})
+
+	t.Run("Get all the Tags check there are only 3", func(t *testing.T) {
+		tags, err := testDB.GetTags(userID, tweetWriter)
+		assert.Nil(t, err, "failed to get tags")
+
+		assert.Equal(t, 4, len(tags), "incorrect number of tags")
+
+		foundTags := []string{}
+		for _, tag := range tags {
+			foundTags = append(foundTags, tag.Tag)
+		}
+
+		assert.Equal(t, []string{"hello", "tree", "tweet", "world"},
+			foundTags, "invalid tweet found")
+	})
+
+	t.Run("Check number of tweetTags", func(t *testing.T) {
+		tags, err := testDB.GetTweetTags(userID, oldTweetID, tweetWriter)
+		assert.Nil(t, err, "failed to get tags")
+
+		assert.Equal(t, 3, len(tags), "incorrect number of tags")
+
+		foundTags := []string{}
+		for _, tag := range tags {
+			foundTags = append(foundTags, tag.Tag)
+		}
+
+		assert.Equal(t, []string{"hello", "tree", "world"},
+			foundTags, "invalid tweet found")
+
+	})
+
 }
