70d1d4d3c5e2c7cd1aa13a35e74be999dff42e7f
param108 <param.ponnaiyan@gmail.com>
2023-05-28 00:52:03 +0530

    A little padding.

--- twitterlike/app/components/editor.tsx
+++ twitterlike/app/components/editor.tsx
@@ -11,7 +11,7 @@ export default function Editor(props: EditorProps) {
         <div className="bg-sky-200 w-[96%] md:w-[510px] rounded mt-[60px] mb-[10px]">
             {props.isLoggedIn ? (
                 <textarea placeholder={"What are you thinking about ?"}
-                    className="block w-[96%] md:w-[500px] h-[150px] resize-none caret-red-500 mt-[5px] mx-[2%] md:mx-[5px] p-[5px] rounded focus:outline-none">
+                    className="block w-[96%] md:w-[500px] h-[150px] resize-none caret-red-500 mt-[5px] mx-[2%] md:mx-[5px] pl-[10px] pr-[5px] py-[5px] rounded focus:outline-none">
                 </textarea>
             ):(
                 <div className="w-[96%] md:w-[500px] h-[150px] bg-white
--- twitterlike/app/components/tweet.tsx
+++ twitterlike/app/components/tweet.tsx
@@ -17,7 +17,7 @@ export default function Tweet(props: TweetProps) {
     return (
             <div className="border border-t-slate-100 
             bg-white hover:bg-cyan-50 w-[90%] md:w-[510px]
-            min-h-[100px] px-[5px] pt-[5px] 
+            min-h-[100px] pl-[15px] pr-[5px] pt-[5px]
             pb-[40px] overflow-x-auto"
                 onClick={()=>{
                     props.router?.push('/tweets/'+props.tweet_id+"/show");
