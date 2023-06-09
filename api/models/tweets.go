95b476cb45261c4c7eba08b6cf03388049680edc
param108 <param.ponnaiyan@gmail.com>
2023-06-09 19:52:55 +0530

    json tags for models

--- api/models/tweets.go
+++ api/models/tweets.go
@@ -3,28 +3,28 @@ package models
 import "time"
 
 type Tweet struct {
-	ID        string `gorm:"default:uuid_generate_v4()"`
-	UserID    string
-	Tweet     string
-	Flags     string
-	Writer    string
-	CreatedAt time.Time
+	ID        string    `gorm:"default:uuid_generate_v4()" json:"id"`
+	UserID    string    `json:"user_id"`
+	Tweet     string    `json:"tweet"`
+	Flags     string    `json:"flags"`
+	Writer    string    `json:"writer"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
 type Tag struct {
-	ID        string `gorm:"default:uuid_generate_v4()"`
-	UserID    string
-	Tag       string
-	Writer    string
-	CreatedAt time.Time
+	ID        string    `gorm:"default:uuid_generate_v4()" json:"id"`
+	UserID    string    `json:"user_id"`
+	Tag       string    `json:"tag"`
+	Writer    string    `json:"writer"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
 type TweetTag struct {
-	ID      int
-	Tag     string
-	TweetID string
-	Writer  string
-	UserID  string
+	ID      int    `json:"id"`
+	Tag     string `json:"tag"`
+	TweetID string `json:"tweet_id"`
+	Writer  string `json:"writer"`
+	UserID  string `json:"user_id"`
 }
 
 type PostTweetsRequest struct {
