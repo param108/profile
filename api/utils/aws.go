5b8ae25379e8ae8ff354897cb41ffe66078992e1
param108 <param.ponnaiyan@gmail.com>
2023-12-10 18:12:36 +0530

    code to get size of each users data

--- api/utils/aws.go
+++ api/utils/aws.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"context"
+	"log"
 	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
@@ -70,4 +71,52 @@ func (awsMgr *AWS) CreateSignedGetUrl(bucket, name string,
 	}
 
 	return request.URL, headers, nil
+}
+
+func (awsMgr *AWS) GetSPBucketSize(bucket, userID string) (uint64, int) {
+
+	prefix := "sp_data_"+ userID
+
+	maxKeys := int(1000)
+
+	cl := s3.NewFromConfig(awsMgr.Config)
+
+	// Set the parameters based on the CLI flag inputs.
+	params := &s3.ListObjectsV2Input{
+		Bucket: &bucket,
+	}
+
+	if len(prefix) != 0 {
+		params.Prefix = &prefix
+	}
+
+	// Create the Paginator for the ListObjectsV2 operation.
+	p := s3.NewListObjectsV2Paginator(cl, params, func(o *s3.ListObjectsV2PaginatorOptions) {
+		if v := int32(maxKeys); v != 0 {
+			o.Limit = v
+		}
+	})
+
+	// Iterate through the S3 object pages, printing each object returned.
+	var i int
+	totalSize := uint64(0)
+	itemCount := int(0)
+	for p.HasMorePages() {
+		i++
+
+		// Next Page takes a new context for each page retrieval. This is where
+		// you could add timeouts or deadlines.
+		page, err := p.NextPage(context.TODO())
+		if err != nil {
+			log.Fatalf("failed to get page %v, %v", i, err)
+		}
+
+		// Log the objects found
+		for _, obj := range page.Contents {
+			totalSize += uint64(obj.Size)
+			itemCount += 1
+		}
+	}
+
+	return totalSize, itemCount
 }
