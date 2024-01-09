db9e285ff7f9e886abb3e6f72925c7318b066598
param108 <param.ponnaiyan@gmail.com>
2024-01-09 21:08:27 +0530

    Adding spiritual practices

--- api/db/scripts/create_services.sql
+++ api/db/scripts/create_services.sql
@@ -2,4 +2,22 @@ INSERT INTO sp_services (id, code, name, category, unit, description, short_desc
 VALUES ('b30ffba1-279b-4825-b349-b57103f3d896', 'MAHAMANTRA', 'Chanting Mahamantra', 'Shravanam', 'rounds', 'Chanting Hare Krsna Mahamantra', 'Chanted {value} rounds of Mahamantra', 'How many rounds?');
 
 INSERT INTO sp_services (id, code, name, category, unit, description, short_description, question)
-VALUES ('0e4e20b1-69ae-45da-8104-fe0ddc34d01a', 'GAYATRI', 'Chanting Gayatri Mantra', 'Shravanam', 'rounds', 'Chanting Hare Krsna Mahamantra', 'Chanted {value} rounds of Mahamantra', 'How many rounds?');
+VALUES ('0e4e20b1-69ae-45da-8104-fe0ddc34d01a', 'GAYATRI', 'Chanting Gayatri Mantra', 'Shravanam', 'rounds', 'Chanting Gayatri', 'Chanted {value} rounds of Mahamantra', 'How many rounds?');
+
+INSERT INTO sp_services (id, code, name, category, unit, description, short_description, question)
+VALUES ('92c43459-96bd-45a5-995d-4d14eb5f834c', 'KIRTAN', 'Singing Kirtan', 'Kirtanam', 'minutes', 'Singing Kirtan for the Lord', 'Sang kirtan for {value} minutes', 'How long did you sing?');
+
+INSERT INTO sp_services (id, code, name, category, unit, description, short_description, question)
+VALUES ('84450562-c5c4-44d0-86a4-5eafdd423629', 'OFFERING', 'Cooking and Offering Prasadam for the Lord', 'Dasyam', 'none', 'Offering Prasadam', 'Cooked Prasadam For the Lord.', 'none');
+
+INSERT INTO sp_services (id, code, name, category, unit, description, short_description, question)
+VALUES ('d08f6b04-d795-4d36-bbb6-f8c78b474036', 'WORSHIPPING', 'Offering Puja to the Lord', 'Arcanam', 'none', 'Performed worship of the Lord', 'Performed Worship of the Lord', 'none');
+
+INSERT INTO sp_services (id, code, name, category, unit, description, short_description, question)
+VALUES ('b4d1098c-820d-4eae-9441-2e5f727c9081', 'PRAYER', 'Offering Prayers to the Lord', 'Vandanam', 'none', 'Offered Prayers to the Lord', 'Offered Prayers to the Lord', 'none');
+
+INSERT INTO sp_services (id, code, name, category, unit, description, short_description, question)
+VALUES ('57534261-b9a5-461c-ab2f-478c9781664f', 'REMEMBERING', 'Remembering the Lord or his pastimes', 'Smaranam', 'none', 'Remembered the Lord', 'Remembered the Lord', 'none');
+
+INSERT INTO sp_services (id, code, name, category, unit, description, short_description, question)
+VALUES ('ddfedfa3-8050-435b-9316-9045b627e703', 'READING_SP_BOOKS', 'Reading Srila Prabhupada''s books', 'Shravanam', 'pages', 'Gained wisdom from Srila Prabhupada''s books', 'Read Srila Prabhupada''s books', 'How many pages?');
