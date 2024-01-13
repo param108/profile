eeacc8b7b6b70f46d439ddd27e52c2cc7a3d1545
param108 <param.ponnaiyan@gmail.com>
2024-01-13 21:22:26 +0530

    Fix the query.

--- api/store/postgres/sp_services.go
+++ api/store/postgres/sp_services.go
@@ -4,7 +4,7 @@ import "github.com/param108/profile/api/models"
 
 func (db *PostgresDB) GetSPServices(writer string) ([]*models.SpService, error) {
 	services := []*models.SpService{}
-	err := db.db.Where("writer = ?").Order("category ASC").Find(&services).Error
+	err := db.db.Where("writer = ?", writer).Order("category ASC").Find(&services).Error
 	if err != nil {
 		return nil, err
 	}
