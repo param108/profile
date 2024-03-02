a3522088ef9301529d83536cae72a75d2a20495a
param108 <param.ponnaiyan@gmail.com>
2024-03-02 11:03:33 +0530

    All users should be added to yatra if it exists.

--- api/store/sp_user.go
+++ api/store/sp_user.go
@@ -1,12 +1,38 @@
 package store
 
 import (
+	"log"
+	"os"
+
 	"github.com/param108/profile/api/models"
 )
 
 func (s *StoreImpl) FindOrCreateSPUser(
 	phone string, writer string) (*models.SpUser, error) {
-	return s.db.FindOrCreateSPUser(phone, writer)
+	spUser, err := s.db.FindOrCreateSPUser(phone, writer)
+	if err != nil {
+		return spUser, err
+	}
+
+	// Add the user to the yatra group
+	// This is a smell, move this out.
+	yatraGrpID := os.Getenv("YATRA_GROUP_ID")
+	if yatraGrpID == "" {
+		return spUser, err
+	}
+
+	if _, err := s.db.GetSPGroupUser(spUser.ID, yatraGrpID, writer); err != nil {
+		if _, err := s.db.AddSPUserToGroup(&models.SpGroupUser{
+			SpGroupID: yatraGrpID,
+			SpUserID:  spUser.ID,
+			Role:      spUser.Role,
+			Writer:    writer,
+		}, writer); err != nil {
+			log.Printf("Failed to add user to yatra: %s\n", err)
+		}
+	}
+
+	return spUser, err
 }
 
 // GetSPUserByID get a sp User by ID
